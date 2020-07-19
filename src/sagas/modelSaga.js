7c3c1bd661cf02cf313ff58294054438080501db
Apoorv <agrawal.av27@gmail.com>
2020-07-20 02:21:24 +0530

    clearing code

--- src/sagas/modelSaga.js
+++ src/sagas/modelSaga.js
@@ -16,8 +16,6 @@ export function* fetchStoryIdsData(action) {
   yield put({ type: FETCH_STORY_DETAIL, storyDetails: data });
 }
 
-// export function* updateStore
-
 export function* loadStoryList() {
   yield takeEvery(LOAD_STORY_LIST_ID, fetchStoryIdsList);
   yield takeEvery(LOAD_STORY_DETAIL, fetchStoryIdsData);
@@ -26,22 +24,3 @@ export function* loadStoryList() {
 export default function* rootSaga() {
   yield all([loadStoryList()]);
 }
-
-// Login Saga: this will be fired on LOGIN_REQUESTED actions
-/* function* onLogin(action) {
-   try {
-      const credentials = yield call(Api.login, action.payload);
-      yield put({type: "LOGIN_SUCCEEDED", credentials: credentials});
-   } catch (e) {
-      yield put({type: "LOGIN_FAILED", message: e.message});
-   }
-} */
-
-/*
-  Starts login on each dispatched `LOGIN_REQUESTED` action.
-*/
-/* function* login() {
-  yield takeEvery("LOGIN_REQUESTED", onLogin);
-} */
-
-// export default NewsSaga;
